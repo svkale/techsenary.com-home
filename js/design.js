1199adce9a813b5e87600ba452be9b34e0a9ba8e
sites-admin <organizer.sites@gmail.com>
2020-11-24 15:03:06 +0530

    v1

--- js/design.js
+++ js/design.js
@@ -19,10 +19,13 @@ for(var i=0;i<nav_elements.length;i++)
 function iframe_start()
 {
 	var iframes_html=document.getElementById('iframe_page');
-	setTimeout(iframe_load,500,iframes_html);
-	setTimeout(iframe_load,1000,iframes_html);
-	setTimeout(iframe_load,2000,iframes_html);
-	setTimeout(iframe_load,5000,iframes_html);
+	if(iframes_html)
+	{
+		setTimeout(iframe_load,500,iframes_html);
+		setTimeout(iframe_load,1000,iframes_html);
+		setTimeout(iframe_load,2000,iframes_html);
+		setTimeout(iframe_load,5000,iframes_html);
+	}
 }
 iframe_start();
 function iframe_load(iframe_obj)
@@ -49,4 +52,34 @@ function iframe_set_height(iframe_obj)
 	{
 		iframe_obj.style.height=iframe_obj.contentDocument.getElementsByTagName("main")[0].scrollHeight+20+"px";
 	}
+}
+
+
+function request(path,func)
+{
+	var param=[];
+	for(var i=2;i<arguments.length;i++)
+	{
+		param[i-2]=arguments[i];
+	}
+	var xmlhttp=new XMLHttpRequest();
+	xmlhttp.onreadystatechange=function()
+	{
+		if (this.readyState==4 && this.status==200)
+		{
+			window[func](this,param);
+		}
+	};
+	xmlhttp.open('GET',path,true); 
+	xmlhttp.send();
+	return;
+}
+function put_hitcount(request_obj,params)
+{
+	var count=JSON.parse(request_obj.responseText.substring(28,request_obj.responseText.length-2)).feed.entry[0].content.$t;
+	params[0].innerHTML=count;
+}
+for(var i=0;i<document.querySelectorAll("[data-ssheet-url]").length;i++)
+{
+	request(document.querySelectorAll("[data-ssheet-url]")[i].getAttribute("data-ssheet-url"),"put_hitcount",document.querySelectorAll("[data-ssheet-url]")[i]);
 }
