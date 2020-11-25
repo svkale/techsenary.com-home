67457ea390c1f96ac8675b2b82a49875e7dfd104
sites-admin <organizer.sites@gmail.com>
2020-11-25 18:22:38 +0530

    v1

--- js/design.js
+++ js/design.js
@@ -16,50 +16,8 @@ for(var i=0;i<nav_elements.length;i++)
 	{
 		nav_elements[i].addEventListener("click",function()
 		{
-			this.parentNode.querySelector(".active").classList.remove("active");
-			this.classList.add("active");
-			history.pushState('','/'+this.getAttribute("data-iframe-url"),'/'+this.getAttribute("data-iframe-url"));
-			document.getElementById('iframe_page').src=window.location+"_f.html";
-			iframe_start();
+			window.location.href="https://"+this.getAttribute("data-iframe-url");
 		});
-	}
-}
-
-function iframe_start()
-{
-	var iframes_html=document.getElementById('iframe_page');
-	if(iframes_html)
-	{
-		setTimeout(iframe_load,500,iframes_html);
-		setTimeout(iframe_load,1000,iframes_html);
-		setTimeout(iframe_load,2000,iframes_html);
-		setTimeout(iframe_load,5000,iframes_html);
-	}
-}
-iframe_start();
-function iframe_load(iframe_obj)
-{
-	iframe_obj.contentWindow.addEventListener("resize",function(){iframe_set_height(this.frameElement);});
-	if(iframe_obj.contentDocument.documentElement)
-	{
-		iframe_obj.style.height=iframe_obj.contentDocument.documentElement.getElementsByTagName("main")[0].scrollHeight+20+"px";
-		iframe_obj.contentDocument.documentElement.getElementsByClassName("background_img")[0].parentNode.style.position="relative";
-	}
-	else
-	{
-		iframe_obj.style.height=iframe_obj.contentDocument.getElementsByTagName("main")[0].scrollHeight+20+"px";
-		iframe_obj.contentDocument.getElementsByClassName("background_img")[0].parentNode.style.position="relative";
-	}
-}
-function iframe_set_height(iframe_obj)
-{
-	if(iframe_obj.contentDocument.documentElement)
-	{
-		iframe_obj.style.height=iframe_obj.contentDocument.documentElement.getElementsByTagName("main")[0].scrollHeight+20+"px";
-	}
-	else
-	{
-		iframe_obj.style.height=iframe_obj.contentDocument.getElementsByTagName("main")[0].scrollHeight+20+"px";
 	}
 }
 
